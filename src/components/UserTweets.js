d0f3fee3c9af3e59d5c7698c973ccca4ee5efd48
Jason Hansen <jason_hansen@outlook.com>
2017-02-10 01:28:28 -0700

    [filtering] step 2, display user tweets

---
+++ src/components/UserTweets.js
@@ -0,0 +1,73 @@
+var React = require('react');
+var Tweet = require('./Tweet');
+var PayloadStates = require('../constants/PayloadStates');
+var Router = require('react-router');
+var InfiniteScrolling = require('../decorators/InfiniteScrolling');
+var LoadMoreButton = require('./LoadMoreButton');
+var _ = require('lodash');
+
+module.exports = lore.connect(function(getState, props){
+  return {
+    tweets: getState('tweet.find', {
+      where: {
+        user: props.params.userId
+      },
+      pagination: {
+        page: props.location.query.page || '1'
+      }
+    })
+  }
+})(
+InfiniteScrolling({ propName: 'tweets', modelName: 'tweet' })(
+React.createClass({
+  displayName: 'UserTweets',
+
+  propTypes: {
+    pages: React.PropTypes.array.isRequired,
+    onLoadMore: React.PropTypes.func.isRequired
+  },
+
+  renderTweet: function(tweet) {
+    return (
+      <Tweet key={tweet.id || tweet.cid} tweet={tweet} />
+    );
+  },
+
+  render: function() {
+    var pages = this.props.pages;
+    var numberOfPages = pages.length;
+    var firstPage = pages[0];
+    var lastPage = pages[pages.length - 1];
+
+    // if we only have one page, and it's fetching, then it's the initial
+    // page load so let the user know we're loading the data
+    if (numberOfPages === 1 && lastPage.state === PayloadStates.FETCHING) {
+      return (
+        <h1 className="loading-text">
+          Loading...
+        </h1>
+      );
+    }
+
+    var tweetListItems = _.flatten(pages.map(function(tweets) {
+      return tweets.data.map(this.renderTweet);
+    }.bind(this)));
+
+    return (
+      <div className="feed">
+        <h2 className="title">
+          My Tweets
+        </h2>
+        <ul className="media-list tweets">
+          {tweetListItems}
+        </ul>
+        <LoadMoreButton
+          lastPage={lastPage}
+          onLoadMore={this.props.onLoadMore}
+          nextPageMetaField="nextPage" />
+      </div>
+    );
+  }
+})
+)
+);
